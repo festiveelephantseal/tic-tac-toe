9e2b7e8006a8b601de1124d015f8f463f2b38cfb
raghav <kenrag24@gmail.com>
2022-05-20 21:25:40 -0500

    It works 🎉

--- main.c
+++ main.c
@@ -16,8 +16,9 @@ int main() {
     char currPlayer = 'O';
 
     int row, col = 0;
+    bool gameFinished = false;
 
-    while (hasBeenWon(board) == false) {
+    while (gameFinished == false) {
         clear();
         printBoard(board);
 
@@ -39,9 +40,17 @@ int main() {
             currPlayer = 'O';
 
         refresh();
+        int status = hasBeenWon(board);
+
+        if (status != 0) {
+            gameFinished = true;
+        }
+ 
     }
     
     endwin();
+
+    getStatus(board);
 
 }
 
@@ -62,22 +71,45 @@ void printBoard(int arr[3][3]) {
     }
 }
 
-bool hasBeenWon(int arr[3][3]) {
+int hasBeenWon(int arr[3][3]) {
     // All horizontal checks
     for (int i = 0; i < 3; i++) {
-        int sum = 0;
-
-        for (int j = 0; j < 3; j++) {
-            sum += arr[i][j];
-        }
-
-        if (sum == 6 || sum == 3) {
-            return true;
+        if (arr[i][0] == arr[i][1] && arr[i][0] == arr[i][2]) {
+            return arr[i][0];
         }
     }
 
+
     // All vertical checks
 
+    for (int i = 0; i < 3; i++) {
+        if (arr[0][i] == arr[1][i] && arr[0][i]== arr[2][i]) {
+            return arr[0][i];
 
-    return false;
+        }
+    }
+
+    // Diagonals check
+    if (arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2]) {
+        return arr[0][0];
+    }
+
+    if (arr[0][2] == arr[1][1] && arr[0][0] == arr[2][0]) {
+         return arr[0][2];
+    }
+
+    return 0;
+}
+
+void getStatus(int arr[3][3]) {
+      int status = hasBeenWon(arr);
+        if (status != 0) {
+            char player;
+            if (status == 1) {
+                player = 'X';
+            } else {
+                player = 'O';
+            }
+            printf("Player %c has won! 🎉\n", player);
+        }
 }
--- tictac.h
+++ tictac.h
@@ -3,4 +3,6 @@
 // Make the board look better later
 void printBoard(int arr[3][3]);
 
-bool hasBeenWon(int arr[3][3]);
+int hasBeenWon(int arr[3][3]);
+
+void getStatus(int arr[3][3]);
